98e6fe969efa84b6f0030b12a3cca2e676a41d10
RFYoung <sc18yy@leeds.ac.uk>
2022-03-08 13:04:43 +0800

    captcha test

---
+++ test.py
@@ -0,0 +1,8 @@
+from ImageCaptchaEnhanced import ImageCaptchaEnhanced
+
+captcha = ImageCaptchaEnhanced(width=192,height=64)
+captcha.write("QO1D", "test/demo1.png")
+captcha.write("OQ1D", "test/demo2.png")
+captcha.write("0D17", "test/demo3.png")
+captcha.write("0Dq9", "test/demo4.png")
+captcha.write("Q96g", "test/demo5.png")
